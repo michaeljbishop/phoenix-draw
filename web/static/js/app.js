d5fcc3701f2dd2e446436642189c283dfad88e55
Michael Bishop <mbtyke@gmail.com>
2016-10-19 13:34:16 -0400

    Add drawing program.

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -19,3 +19,94 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+var canvas = document.getElementById("draw-canvas");
+var ctx = canvas.getContext("2d");
+
+function drawLine(from, to) {
+  ctx.moveTo(from.x, from.y);
+  ctx.lineTo(to.x, to.y);
+  ctx.stroke();
+}
+
+// ------------------------
+//  General Input Tracking
+// ------------------------
+
+var lastPoints = {};
+
+function moveTo(identifier, point) {
+  lastPoints[identifier] = point;
+}
+
+function lineTo(identifier, point) {
+  if (lastPoints[identifier]) {
+    drawLine(lastPoints[identifier], point);
+  }
+  lastPoints[identifier] = point;
+}
+
+function getPos(client) {
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: client.clientX - rect.left,
+    y: client.clientY - rect.top
+  };
+}
+
+function haltEventBefore(handler) {
+  return function(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    handler(event);
+  }
+}
+
+// ----------------
+//  Touch Handling
+// ----------------
+
+var mouseDown = false;
+
+canvas.addEventListener('mousedown', haltEventBefore(function(event) {
+  mouseDown = true;
+  moveTo("mouse", getPos(event));
+}));
+
+// We need to be able to listen for mouse ups for the entire document
+document.documentElement.addEventListener('mouseup', function(event) {
+  mouseDown = false;
+});
+
+canvas.addEventListener('mousemove', haltEventBefore(function(event) {
+  if (!mouseDown) return;
+  lineTo("mouse", getPos(event));
+}));
+
+canvas.addEventListener('mouseleave', haltEventBefore(function(event) {
+  if (!mouseDown) return;
+  lineTo("mouse", getPos(event));
+}));
+
+canvas.addEventListener('mouseenter', haltEventBefore(function(event) {
+  if (!mouseDown) return;
+  moveTo("mouse", getPos(event));
+}));
+
+// ----------------
+//  Touch Handling
+// ----------------
+
+function handleTouchesWith(func) {
+  return haltEventBefore(function(event) {
+    for (var i = 0; i < event.changedTouches.length; i++) {
+      var touch = event.changedTouches[i];
+      func(touch.identifier, getPos(touch));
+    }
+  });
+};
+
+canvas.addEventListener('touchstart',  handleTouchesWith(moveTo));
+canvas.addEventListener('touchmove',   handleTouchesWith(lineTo));
+canvas.addEventListener('touchend',    handleTouchesWith(lineTo));
+canvas.addEventListener('touchcancel', handleTouchesWith(moveTo));
