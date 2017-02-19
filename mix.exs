65ca1fd9193f71703de66888963d68e969012232
Michael Bishop <mbtyke@gmail.com>
2017-02-19 07:10:45 -0500

    Updated Elixir

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Draw.Mixfile do
   def project do
     [app: :draw,
      version: "0.0.1",
-     elixir: "~> 1.2",
+     elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
